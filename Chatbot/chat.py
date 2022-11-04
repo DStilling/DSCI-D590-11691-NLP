17760917e6f018fb304907092b93ec93ec200c80
Seth <76740071+DStilling@users.noreply.github.com>
2022-11-03 21:43:48 -0400

    Add files via upload

---
+++ Chatbot/chat.py
@@ -0,0 +1,30 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+class Chat_T:
+    chatbot: None
+
+    def __init__(self):
+        self.chatbot = ChatBot("ChatBot")
+    # Training with Personal Ques & Ans
+        conversation = [
+            "Hello",
+            "Hi there!",
+            "How are you doing?",
+            "I'm doing great.",
+            "You're welcome.",
+            "who Developed you",
+            "NLP class",
+            "NLP is amazing.",
+            "Do you know Alexa?",
+            "Carl Sagan is the OG!",
+            "I want to vote someday.",
+            "I doubt it."
+        ]
+        trainer = ListTrainer(self.chatbot)
+        trainer.train(conversation)
+
+    def chat(self, t1):
+        response = self.chatbot.get_response(t1)
+        return response
